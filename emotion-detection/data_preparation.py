e09590a13da494cd114d95ba1211e679277e7a21
chris <cjfiel322@gmail.com>
2025-11-14 13:57:42 +0800

    Finished facial emotion detection project

---
+++ emotion-detection/data_preparation.py
@@ -0,0 +1,18 @@
+import os
+import cv2
+import numpy as np
+from utils import get_face_landmarks
+
+data_dir = "facial_emotion_dataset/"
+output_file = "data.txt"
+
+with open(output_file, "w") as f:
+    for emotion_indx, emotion in enumerate(sorted(os.listdir(data_dir))):
+        for image_name in os.listdir(os.path.join(data_dir, emotion)):
+            image_path = os.path.join(data_dir, emotion, image_name)
+            image = cv2.imread(image_path)
+
+            face_landmarks = get_face_landmarks(image)
+            if len(face_landmarks) == 1404:
+                face_landmarks.append(int(emotion_indx))
+                f.write(",".join(map(str, face_landmarks)) + "\n")
